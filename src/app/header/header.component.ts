700b24885d4f07c62c58a77f29e77dee3a5e0567
TIJOTOMY <tijotomy247@gmail.com>
2021-07-25 21:35:10 +0530

    header change

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {DataServiceService} from "../service/data-service.service"
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+public headerNav:Array<any>=[];
+  constructor(private dataservice:DataServiceService) { }
 
   ngOnInit(): void {
+    this.dataservice.getHeaderData().subscribe(data=>{
+      this.headerNav=data;
+      console.log(data)
+    })
   }
 
 }
